3d193083daf1226a9b764db3e18f025735840ffa
w-phil <wphillips892@gmail.com>
2021-09-01 18:52:41 -0400

    more forms

---
+++ forms/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import UserForm from './components/UserForm';
+import './App.css';
+
+function App() {
+  const [state, setState] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  });
+  return (
+    <div className="App">
+      <UserForm inputs={state} setInputs={setState} />
+    </div>
+  );
+}
+
+export default App;
