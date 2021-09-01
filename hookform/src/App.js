d8c8d02caffe23142b0c000f06647f15636c69ba
w-phil <wphillips892@gmail.com>
2021-09-01 14:38:10 -0400

    hookform

---
+++ hookform/src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import UserForm from './components/UserForm';
+import Results from './components/Results';
+import './App.css';
+
+function App() {
+  const [state, setState] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  });
+  return (
+    <div className="App">
+      <UserForm inputs={state} setInputs={setState} />
+      <Results data={state} />
+    </div>
+  );
+}
+
+export default App;
---
+++ hookform/src/components/UserForm.js
@@ -0,0 +1,40 @@
+import React from  'react';
+    
+const UserForm = props => {
+    const { inputs, setInputs } = props;
+    
+    const onChange = e => {
+        setInputs({
+            ...inputs,
+            [e.target.name]: e.target.value
+        });
+    };
+    
+    return(
+        <form>
+            <div>
+                <label>First Name: </label> 
+                <input onChange={onChange} type="text" name="firstName" />
+            </div>
+            <div>
+                <label>Last Name: </label> 
+                <input onChange={onChange} type="text" name="lastName" />
+            </div>
+            <div>
+                <label>Email Address: </label> 
+                <input onChange={onChange} type="text" name="email" />
+            </div>
+            <div>
+                <label>Password: </label>
+                <input onChange={onChange} type="text" name="password" />
+            </div>
+            <div>
+                <label>Confirm Password: </label>
+                <input onChange={onChange} type="text" name="confirmPassword" />
+            </div>
+            <input type="submit" value="Create User" />
+        </form>
+    );
+};
+    
+export default UserForm;
