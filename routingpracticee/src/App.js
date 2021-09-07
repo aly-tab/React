c3e306208cdcdca75fd19d0cf567f514d90c54e5
w-phil <wphillips892@gmail.com>
2021-09-07 13:51:58 -0400

    routingpractice

--- routingpracticee/src/App.js
+++ routingpracticee/src/App.js
@@ -13,28 +13,15 @@ const Home = (props) => {
     <h1>Welcome</h1>
   );
 }
-    
+
 const Hello = (props) => {
   const { val } = useParams();
-  return (
-    <h1>The word is: {val}</h1>
-  );
-}
-
-const Four = (props) => {
-  const { val } = useParams();
-  return (
-    <h1>The number is: {val}</h1>
-  );
-}
-
-const WordColor = (props) => {
-  const { word } = useParams();
   const { color1 } = useParams();
   const { color2 } = useParams();
   return (
-    <h1 style={{color: {color1}, backgroundColor: {color2}}}>{word}</h1>
-    //<h1 style={{color: "red", backgroundColor: "blue"}}>hello</h1>
+    isNaN(val) === true?
+    <h1 style={{color: color1, backgroundColor: color2}}>The word is: {val}</h1> :
+    <h1 style={{color: color1, backgroundColor: color2}}>The number is: {val}</h1> 
   )
 }
     
@@ -42,14 +29,11 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/:val" render={ (props) => {
-          if (isNaN(props.match.params.val) === true) {
-            <Hello /> } else {
-            <Four /> }
-          }}>
+        <Route exact path="/:val">
+          <Hello/>
         </Route>
-        <Route path="/:word/:color1/:color2">
-          <WordColor/>
+        <Route exact path="/:val/:color1/:color2">
+          <Hello/>
         </Route>
         <Route exact path="/">
           <Home />
